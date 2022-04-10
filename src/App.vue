f9b157e13d615a06af77d6cc466d427dfe9ed8fe
Daria <gorbatyukdaria@gmail.com>
2022-04-10 17:55:27 +0300

    Start crm (2 lesson)

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,27 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div id="app">
+    <component :is="layout">
+      <router-view></router-view>
+    </component>
+  </div>
 </template>
+<script>
+import EmptyLayout from "@/layout/EmptyLayout";
+import MainLayout from "@/layout/MainLayout";
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  components:{
+    EmptyLayout,
+    MainLayout,
+  },
+  computed: {
+    layout(){
+      return (this.$route.meta.layout ?? 'empty') + '-layout'
     }
   }
 }
+</script>
+<style lang="scss">
+@import "~materialize-css/dist/css/materialize.min.css";
+@import './assets/index.css';
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,32 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-
+import Home from '../views/Home.vue'
+import Login from '../views/Login'
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  }
+    path: '/login',
+    name: 'login',
+    meta: {layout: 'empty'},
+    component: ()=>import('../views/Login')
+  },
+  {
+    path: '/categories',
+    name: 'categories',
+    meta: {layout: 'main'},
+    component: ()=>import('../views/Categories')
+  },
+  // {
+  //   path: '/about',
+  //   name: 'about',
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
