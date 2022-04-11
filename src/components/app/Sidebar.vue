6c493af7121d3e3fe6a75f8acb9c00a9982ca518
Daria <gorbatyukdaria@gmail.com>
2022-04-11 20:59:38 +0300

    Добавляет sidebar

---
+++ src/components/app/Sidebar.vue
@@ -0,0 +1,54 @@
+<template>
+  <ul class="sidenav app-sidenav open">
+    <router-link v-for="link in links"
+                 :key="link.pathName"
+                 active-class="active"
+                 :to="{name: link.pathName}"
+                 custom
+                 v-slot="{ navigate, href, route, isActive, isExactActive }"
+    >
+      <li  :class="[isExactActive && 'active']">
+        <a :href="href" class="waves-effect waves-orange pointer"
+           @click="navigate">{{link.title}}</a>
+      </li>
+    </router-link>
+
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "Sidebar",
+  setup() {
+    const links = [
+      {
+        title: 'Счет',
+        pathName: 'home'
+      },
+      {
+        title: 'История',
+        pathName: 'history'
+      },
+      {
+        title: 'Планирование',
+        pathName: 'planning'
+      },
+      {
+        title: 'Новая запись',
+        pathName: 'record'
+      },
+      {
+        title: 'Категории',
+        pathName: 'categories'
+      },
+    ]
+    return {
+      links
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
